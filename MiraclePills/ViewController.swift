0cb7d2346f47b7a63b8ef263258bd8b7ed666421
yugsalvation <33853459+yugsalvation@users.noreply.github.com>
2020-04-25 14:04:02 -0400

    submit project

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+extension UIViewController {
+func dismissKey()
+{
+let tap: UITapGestureRecognizer = UITapGestureRecognizer( target: self, action: #selector(UIViewController.dismissKeyboard))
+tap.cancelsTouchesInView = false
+    view.addGestureRecognizer(tap)
+}
+@objc func dismissKeyboard()
+{
+view.endEditing(true)
+}
+}
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    @IBOutlet weak var submitBtn: UIButton!
+    let states=["Ontario","Qubec","Nova Scotia","Alberta"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource=self
+        statePicker.delegate=self
+        self.dismissKey()
         // Do any additional setup after loading the view.
+     //   self.view.backgroundColor=UIColor.purple
     }
 
 
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        statePicker.isHidden=false
+        submitBtn.isHidden=true
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for:UIControl.State.normal)
+        statePicker.isHidden=true
+        submitBtn.isHidden=false
+    }
 }
 
